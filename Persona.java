f58510957c47bad2f696648d1d5e7bacf319cb72
Espadaclar <eneroborde8002@gmail.com>
2017-02-10 12:09:03 +0100

    mt getAlimentoMasCaloricoConsumido() imprime en pantalla el nombre del alimento más calórico, o null sin no ha comido.

--- Persona.java
+++ Persona.java
@@ -12,6 +12,9 @@ class Persona {
     private int altura;
     private int edad;
     private int caloriasTotales;// suma las calorias de varias comidas.
+    
+    private String nameComidaMasCalorica; // nombre de la comida con más calorias.
+    private Comida comidaCalorica;
 
     public Persona(String name, boolean isHombre, int peso, int altura, int edad) {
         this.name = name;
@@ -20,6 +23,9 @@ class Persona {
         this.altura = altura;
         this.edad = edad;
         caloriasTotales = 0;
+        
+        nameComidaMasCalorica = "";
+        comidaCalorica = null; // la doy valor en  el mt comer().
     }
 
     public String getName() {
@@ -49,8 +55,15 @@ class Persona {
      *en caso de que no la coma.
      */
     public int comer(Comida comida){
+        
+        comidaCalorica = new Comida(comida.getNameComida(), comida.getCaloriasPorComida());        
+        if(comida.getCaloriasPorComida() >= comidaCalorica.getCaloriasPorComida()){
+           comidaCalorica = comida; // de esta manera el atributo 'Comida comidaCalorica' almacena siempre la comida con más calorías;
+           nameComidaMasCalorica = comidaCalorica.getNameComida();// útil para poder ejecutar el mt getAlimentoMasCaloricoConsumido();
+        }                           
+        
         int caloriasInge = -1;
-        if(comida != null && caloriasTotales < calculoMetabolismoBasal()){
+        if(caloriasTotales < calculoMetabolismoBasal()){
             caloriasInge = comida.getCaloriasPorComida();
             caloriasTotales = caloriasTotales + caloriasInge; // suma las calorias de varias comidas.
         }
@@ -97,21 +110,6 @@ class Persona {
      * contestar, admite un único parámetro y debe devolver la respuesta además
      * de imprimirla por pantalla.
      */
-    /**
-     * Ahora queremos que sea posible preguntarle cosas a la persona. Si no ha sobrepasado 
-     * su metabolismo basal, te contestará "SI" o "NO" (¡en mayúsculas!) dependiendo de si 
-     * la pregunta tiene una longitud (es decir, el número de letras de la misma) 
-     * divisible por 3 o no, respectivamente.
-
-     *En caso de que la persona ya haya sobrepasado el metabolismo basal o en el caso de 
-     *que tu pregunta contenga el nombre de la persona, responderá con la misma pregunta 
-     *que le has hecho pero gritando (es decir, el mismo texto de la pregunta pero
-     *en mayúsculas) indiferentemente de la longitud de a pregunta.
-     *
-     * El método que se utiliza para preguntar cosas a la persona debe llamarse 
-     * contestar, admite un único parámetro y debe devolver la respuesta además
-     * de imprimirla por pantalla.
-     */
     public String contestar(String pregunta){
         String respuesta = pregunta;
         int totalDeLetras = 0; // almacenará el nº total de letras que tine la pregunta.
@@ -135,5 +133,33 @@ class Persona {
         }
         return respuesta;
     }
+    
+    /**
+     * Se pide que implementes el código necesario para disponer de un método llamado getAlimentoMasCaloricoConsumido
+     * que imprima por pantalla y que devuelva el nombre de la comida más calórico ingerida hasta ahora por un usuario.
+
+     *En caso de que la persona no haya comido nada el método infoma por pantalla de tal situación y devuelve null. 
+     * En caso de que haya empate entre dos o más comidas, imprime y devuelve la última de ellas.
+     */
+    public String getAlimentoMasCaloricoConsumido() {
+        String nameComida = null;
+        if (caloriasTotales > 0) {
+            //nameComidaMasCalorica, es un atributo que va almacenando el nombre de la comida más calórica dentro del mt comer().
+            nameComida = nameComidaMasCalorica;
+        }
+        else{
+            System.out.println("No ha tomado ninguna comida.");
+        }
+        return nameComida;
+    }
 }
+
+
+
+
+
+
+
+
+
 
